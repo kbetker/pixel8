4f9f06f459c19922eb4396ef08b05544fc38a928
Zach <zach.m.yu@gmail.com>
2021-05-04 10:21:42 -0700

    Updated Auth paths / in process

--- express-project-starter/express-project-starter/app.js
+++ express-project-starter/express-project-starter/app.js
@@ -1,50 +1,49 @@
 const createError = require('http-errors');
 const express = require('express');
+const morgan = require('morgan');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const { sequelize } = require('./db/models');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const { sequelize } = require('./db/models');
+const { environment, sessionSecret } = require('./config');
+const { restoreUser } = require('./auth');
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
 
-// view engine setup
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+app.use(morgan('dev'));
+app.use(cookieParser(sessionSecret));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// set up session middleware
-const store = new SequelizeStore({ db: sequelize });
-
-app.use(
-  session({
-    secret: 'superSecret',
-    store,
-    saveUninitialized: false,
-    resave: false,
-  })
-);
-
-// create Session table if it doesn't already exist
-store.sync();
-
+// app.use(restoreUser);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
+const store = new SequelizeStore({ db: sequelize });
+store.sync();
+app.use(session({
+  secret: sessionSecret,
+  store,
+  saveUninitialized: false,
+  resave: false,
+})
+);
+
+app.use((req, res, next) => {
+  const err = new Error(`The requested page couldn't be found.`);
+  err.status = 404;
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- express-project-starter/express-project-starter/routes/users.js
+++ express-project-starter/express-project-starter/routes/users.js
@@ -1,13 +1,65 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt')
+const { asyncHandler, csrfProtection } = require('../utils.js')
+const { check, validationResult } = require('express-validator')
+const { loginUser, logoutUser } = require('../auth');
+
+const loginValidators = [
+  check('Username')
+    .exists({ checkFalsy: true })
+    .withMessage('Please enter your Username')
+    .isLength({ max: 255 })
+    .withMessage('Username must not be more than 255 characters long'),
+  check('Password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please enter you password')
+    .isLength({ max: 100 })
+    .withMessage('Password must not be more than 100 characters long')
+];
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', csrfProtection, asyncHandler(async (req, res, next) => {
   res.send('respond with a resource');
-});
+}));
 
-router.get('/login', function(req, res, next) {
-  res.render('users-login');
+router.get('/login', csrfProtection, asyncHandler(async (req, res, next) => {
+  res.render('users-login', { csrfToken: req.csrfToken() });
+}));
+
+router.post('/login', csrfProtection, loginValidators, asyncHandler(async (req, res, next) => {
+  const {
+    username,
+    password,
+  } = req.body;
+
+  let errors = [];
+  const validatorErrors = validationResult(req);
+
+  if (validatorErrors.isEmpty()) {
+    const user = await db.Pixel_User.findOne({ where: { username } });
+    if (user !== null) {
+      const passwordMatch = await bcrypt.compare(password, user.hashedPassword.toString());
+
+      if (passwordMatch) {
+        loginUser(req, res, user);
+        return res.redirect('/');
+      }
+    }
+
+    errors.push('Login failed for the provided username and password');
+  } else {
+    errors = validatorErrors.array().map((error) => error.msg);
+  }
+
+  res.render('users-login', username, errors, { csrfToken: req.csrfToken() });
+}));
+
+
+
+router.post('/logout', (req, res) => {
+  logoutUser(req, res);
+  res.redirect('/');
 });
 
 module.exports = router;
