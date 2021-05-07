ced68dadf56a703b6df204c6f0d8e1cd6c17c46a
kbetker <kevin@betker.org>
2021-05-06 19:29:18 -0500

    added heart.js

---
+++ public/javascripts/heart.js
@@ -0,0 +1,26 @@
+window.addEventListener("load", (event)=>{
+    //===================  Heart Button ===============================================
+    let pixelHeartContainer = document.getElementById("heart_container");
+    let pixelHeart =  document.getElementById("pixel__heart");
+    let pixelHeartShadow = document.getElementById("pixelHeart-shadow");
+    let isHearted = false;
+
+    function checkHeart(){
+        if(isHearted){
+            pixelHeart.classList.remove("pixel__heart-slideout");
+            pixelHeart.classList.add("pixel__heart-slidein")
+            pixelHeartShadow.classList.add("pixelHeart-shadow")
+        } else {
+            pixelHeart.classList.add("pixel__heart-slideout");
+            pixelHeart.classList.remove("pixel__heart-slidein")
+            pixelHeartShadow.classList.remove("pixelHeart-shadow")
+        }
+    }
+    pixelHeartContainer.addEventListener("click", event => {
+        isHearted ? isHearted = false : isHearted = true;
+        checkHeart();
+    })
+    const heartInit = () => isHearted ? checkHeart() : console.log('not hearted')
+    heartInit();
+
+});
