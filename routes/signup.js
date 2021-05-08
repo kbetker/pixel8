1499461e3ef1078f2b109a2f75e3f9a617a71eeb
Zach <zach.m.yu@gmail.com>
2021-05-08 12:00:33 -0700

    Removed password requirements for sign-up

--- routes/signup.js
+++ routes/signup.js
@@ -45,9 +45,9 @@ const userValidators = [
         .exists({ checkFalsy: true })
         .withMessage('Please provide a value for Password')
         .isLength({ max: 255 })
-        .withMessage('Password must not be more than 255 characters long')
-        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, 'g')
-        .withMessage('Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'),
+        .withMessage('Password must not be more than 255 characters long'),
+    // .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, 'g')
+    // .withMessage('Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'),
     check('confirmPassword')
         .exists({ checkFalsy: true })
         .withMessage('Please provide a value for Confirm Password')
